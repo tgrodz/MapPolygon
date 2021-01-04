0f1a8147cae2fe167dc15c4070be5e8df69b322f
tgrodz <56519091+tgrodz@users.noreply.github.com>
2021-01-05 01:37:51 +0200

    Update AreaApi.kt
    ref

--- app/src/main/java/com/tgrodz/mappolygon/network/AreaApi.kt
+++ app/src/main/java/com/tgrodz/mappolygon/network/AreaApi.kt
@@ -5,7 +5,7 @@ import io.reactivex.Observable
 import retrofit2.Call
 import retrofit2.http.*
 
-//https://api.kernel.ua/api_esb/AgileRider/api/GetCornFieldsByDevice?dev_ID=383722E7-F691-44FA-856B-6118B34CF4AB
+
 interface AreaApi {
 
     @Headers("Accept: application/json", "Content-type:application/json")
